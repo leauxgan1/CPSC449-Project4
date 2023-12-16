cd329181ac267dba6e6fae1e8dd4b45158b98267
janiljohn <aniljohnjoel@gmail.com>
2023-12-16 02:17:58 -0800

    implemented notification via webhook

--- backend-project2/enrollment_service/notification_consumer.py
+++ backend-project2/enrollment_service/notification_consumer.py
@@ -31,17 +31,26 @@ def get_email(message: str):
     subscriptions = r.lrange(email_key, 0, -1)
     return (subscriptions[0]).decode("utf-8")[2:]
 
-
 def callback_webhook(ch, method, properties, body):
     webhook_url = get_webhook(body.decode("utf-8"))
-    web = httpx.post(usr_url, data={'message': 'Hello, world!'})
-    print(web.text)
+    body_str = body.decode("utf-8")
+    student_id = body_str[0:body_str.find("_")]
+    class_id = body_str[body_str.find("_")+1:]
+    try:
+        response = httpx.post(webhook_url, data={'message': f'{student_id} was successfully enrolled into class {class_id}'})
+        response.raise_for_status()
+        print(response.status_code)
+    except httpx.HTTPError as exc:
+        ch.basic_nack(delivery_tag=method.delivery_tag)
+        print(f"HTTP Exception for {exc.request.url} - {exc}")
+        return
 
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def callback_email(ch, method, properties, body):
     email = get_email(body.decode("utf-8"))
 
-channel.basic_consume(queue=queue_name_webhook, on_message_callback=callback_webhook, auto_ack=True)
+channel.basic_consume(queue=queue_name_webhook, on_message_callback=callback_webhook)
 channel.basic_consume(queue=queue_name_email, on_message_callback=callback_email, auto_ack=True)
 
 channel.start_consuming()
