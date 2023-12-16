b0f35124c47bcb67b1ac1a5fdfedf02c6a473784
Ampersand-Alexander <student@tuffix-vm.myguest.virtualbox.org>
2023-12-15 21:11:03 -0800

    rabbitmq

--- backend-project2/enrollment_service/notification_helper.py
+++ backend-project2/enrollment_service/notification_helper.py
@@ -9,6 +9,7 @@ def send_rabbitmq_message(exchange_name, message):
     channel.basic_publish(exchange=exchange_name, routing_key='', body=message)
     connection.close()
 
+
 def consume_rabbitmq_message():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
@@ -22,24 +23,5 @@ def consume_rabbitmq_message():
 
     channel.start_consuming()
 
-def send_email_notification(to_email, subject, body):
-    from_email = 'your_email@example.com'  # replace with your email
-    smtp_server = 'localhost'
-    smtp_port = 8025
-
-    message = MIMEMultipart()
-    message['From'] = from_email
-    message['To'] = to_email
-    message['Subject'] = subject
-
-    # Attach the body of the email
-    message.attach(MIMEText(body, 'plain'))
-
-    try:
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.sendmail(from_email, to_email, message.as_string())
-    except Exception as e:
-        print(f"Error sending email: {e}")
-
-http://localhost:5500
-r = httpx.post('https://httpbin.org/post', data={'key': 'value'})
+#http://localhost:5500
+#r = httpx.post('https://httpbin.org/post', data={'key': 'value'})
